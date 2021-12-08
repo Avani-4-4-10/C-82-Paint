772c62cff47de9597d8d5daf48510a1788da963d
Avani-4-4-10 <92733721+Avani-4-4-10@users.noreply.github.com>
2021-12-08 18:57:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var mouseEvent="";
+var last_position_of_X,last_position_of_Y
+var Canvas= document.getElementById("myCanvas")
+var ctx= Canvas.getContext("2d")
+var color="purple"
+var width=2
+
+Canvas.addEventListener("mousedown",my_mousedown)
+function my_mousedown(){
+    mouseEvent="mousedown"
+}
+Canvas.addEventListener("mouseup",my_mouseup)
+function my_mouseup(){
+    mouseEvent="mouseup"
+}
+Canvas.addEventListener("mouseleave",my_mouseleave)
+function my_mouseleave(){
+    mouseEvent="mouseleave"
+}
+Canvas.addEventListener("mousemove",my_mousemove)
+function my_mousemove(e){
+    current_Position_of_X=e.clientX-Canvas.offsetLeft
+    current_Position_of_Y=e.clientY-Canvas.offsetTop
+    if(mouseEvent=="mousedown"){
+        ctx.beginPath()
+        ctx.lineWidth=width;
+        ctx.strokeStyle=color;
+        ctx.moveTo(last_position_of_X,last_position_of_Y)
+        ctx.lineTo(current_Position_of_X,current_Position_of_Y)
+        ctx.stroke()
+    }
+    last_position_of_X=current_Position_of_X
+    last_position_of_Y=current_Position_of_Y
+}
